72b0b33fa61515345775c6c088af1c7c0d2d6b10
David Kayser <DavidKayser@users.noreply.github.com>
2022-03-21 15:53:17 -0700

    add css styling to main reddit section

--- src/features/reddits/Reddits.js
+++ src/features/reddits/Reddits.js
@@ -1,24 +1,27 @@
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { selectReddits } from "./redditsSlice";
+import './Reddits.css';
 
 export default function Reddits() {
   const reddits = useSelector(selectReddits);
   return (
-    <section className="reddits">
-        <h1>Reddits</h1>
+    <section className="main-content">
+        <h2>Popular Posts</h2>
         <div className="reddits-previews">
             {Object.values(reddits).map((reddit) => (
-                <article key={reddit.id}>
+                <article className="reddit-article" key={reddit.id}>
+                    <div className="reddit-header">
+                        <img src={reddit.authorImage} alt={reddit.author} className="author-image float-left" />
+                        <p className="float-left community">{reddit.community}</p>
+                        <p className="float-left">Posted by {reddit.author}</p>
+                    </div>
                     <h3>{reddit.title}</h3>
-                    <img src={reddit.image} alt={reddit.title} />
-                    <div className="redditFooter">
-                        <div className="reddit-author">
-                            <img src={reddit.authorImage} alt={reddit.author} />
-                            <p>{reddit.author}</p>
-                        </div>
-                        <p>{reddit.postedOn}</p>
-                        <p>{reddit.numberOfComments} Comments</p>
+                    <img className="reddit-image" src={reddit.image} alt={reddit.title} />
+                    <div className="reddit-footer">
+                        <p className="float-left">{reddit.upvotes} upvotes</p>
+                        <p className="float-left">{reddit.numberOfComments} Comments</p>
+                        <p className="float-left">{reddit.postedOn}</p>
                     </div>
                 </article>
             ))}
--- src/features/reddits/redditsSlice.js
+++ src/features/reddits/redditsSlice.js
@@ -6,18 +6,22 @@ const redditsSlice = createSlice({
         reddits: [
             {
                 id: 1,
+                community: "r/something",
                 title: "Test Title",
-                image: "https://b.thumbs.redditmedia.com/VUqn5Ks9yX4f4pbLMk6ESjN-1QuJSLr51YWBHXu_BIk.jpg",
+                image: "https://i.redd.it/f6b7703uwko81.gif",
                 author: "David K",
                 authorImage: "https://b.thumbs.redditmedia.com/VUqn5Ks9yX4f4pbLMk6ESjN-1QuJSLr51YWBHXu_BIk.jpg",
+                upvotes: "34.3k",
                 postedOn: "Jan 2nd, 2012"
             },
             {
                 id: 2,
+                community: "r/somethingElse",
                 title: "Test Title",
-                image: "https://b.thumbs.redditmedia.com/VUqn5Ks9yX4f4pbLMk6ESjN-1QuJSLr51YWBHXu_BIk.jpg",
+                image: "https://i.imgur.com/tdiOx5E.jpg",
                 author: "David K",
                 authorImage: "https://b.thumbs.redditmedia.com/VUqn5Ks9yX4f4pbLMk6ESjN-1QuJSLr51YWBHXu_BIk.jpg",
+                upvotes: "3",
                 postedOn: "Jan 2nd, 2012"
             }
         ]
