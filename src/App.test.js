1b76d76ab4efba78133df9f88df9f0da4c2e63be
David Kayser <DavidKayser@users.noreply.github.com>
2022-03-21 19:10:35 -0700

    Reverting to 72b0b33fa61515345775c6c088af1c7c0d2d6b10

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import { Provider } from 'react-redux';
+import store from './app/store';
+import App from './app/App';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  render(<Provider store={store} ><App /></Provider>);
+  const linkElement = screen.getByText(/popular/i);
   expect(linkElement).toBeInTheDocument();
 });
