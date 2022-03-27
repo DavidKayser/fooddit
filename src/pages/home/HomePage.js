255e0c244d3837f6b0a27dbdf882582088e9c7c5
David Kayser <DavidKayser@users.noreply.github.com>
2022-03-27 14:30:15 -0700

    add filter sidebar to homepage

--- src/pages/home/HomePage.js
+++ src/pages/home/HomePage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './HomePage.css';
 import Reddits from '../../features/reddits/Reddits'; 
+import Sidebar from '../../components/sidebar/Sidebar'; 
 
 const HomePage = () => {
     return (
         <div id="home-page" className="page">
             <Reddits />
+            <Sidebar />
         </div>
     );
 }
