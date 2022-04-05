991456c0e110fa26df575af68b07a17fd744ec5b
David Kayser <DavidKayser@users.noreply.github.com>
2022-04-05 14:31:51 -0700

    add subComments to display

--- src/features/comments/Comments.js
+++ src/features/comments/Comments.js
@@ -24,6 +24,7 @@ const Comments = () => {
             <h3 id="comments-title">Comments</h3>
             <ul className="comments-list">
             {Object.values(comments).map((comment, index) => (
+                <div>
                     <article className="comments" key={index}>
                         <div className="comment-header">
                             <p className="float-left">{comment.name}</p>
@@ -35,6 +36,21 @@ const Comments = () => {
                             <p className="float-left">{comment.downVotes}</p>
                         </div>
                     </article>
+                    {/* {comments.subComments &&
+                    Object.values(comments.subComments).map((comment, index) => (
+                        <article className="comments" key={index + 1000}>
+                            <div className="comment-header">
+                                <p className="float-left">{comment.name}</p>
+                                <p className="float-left">{comment.postedOn}</p>
+                            </div>
+                            <p>{comment.comment}</p>
+                            <div className="comment-footer">
+                                <p className="float-left">{comment.upVotes}</p>
+                                <p className="float-left">{comment.downVotes}</p>
+                            </div>
+                        </article>
+                    ))} */}
+                </div>
                 ))}
             </ul>
         </section>
